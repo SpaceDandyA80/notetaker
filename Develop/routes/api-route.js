f2a0afd46c1f84a1fdaa2db00221898b85339928
SpaceDandyA80 <58712760+SpaceDandyA80@users.noreply.github.com>
2020-06-17 22:41:06 -0700

    fs module

--- Develop/routes/api-route.js
+++ Develop/routes/api-route.js
@@ -1,4 +1,5 @@
 const dataBase = require('../db/db.json');
+const fs = require('fs');
 // const waitListData = require('../data/waitingList-data.js');
 
 module.exports = function (app){
@@ -7,12 +8,20 @@ module.exports = function (app){
         res.json(dataBase);
     });
     app.post('/api/notes', function (req, res){
-        console.log(req.body);
+       // console.log(req.body);
         dataBase.push(req.body)
-        console.log(dataBase);
+
+       // console.log(dataBase);
 
         res.json(dataBase);
+
+        fs.readFile('./db/db.json','utf8', (err, data) => {
+            if (err) throw err;
+            console.log(data);
+          });
     });
+    
+
 
     app.delete('/api/notes/:id', function(req,res){
       console.log( req.params.id)
